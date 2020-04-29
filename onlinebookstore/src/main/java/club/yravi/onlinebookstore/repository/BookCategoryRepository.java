05648011bd0e61166279d139ee392bcd51762db1
yraviui <yravi.ui@gmail.com>
2020-04-28 18:46:53 -0700

    Completed the backend for feature one.

---
+++ onlinebookstore/src/main/java/club/yravi/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,11 @@
+package club.yravi.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import club.yravi.onlinebookstore.entity.BookCategory;
+
+@RepositoryRestResource(collectionResourceRel = "bookCategory", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
+	
+}
